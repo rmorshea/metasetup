e0ceb3586acd0977e9f2fa5aef538524f4279be6
Ryan Morshead <ryan.morshead@gmail.com>
2018-02-18 23:18:59 -0800

    fix all the broken stuff

--- metasetup/_metasetup.py
+++ metasetup/_metasetup.py
@@ -1,10 +1,19 @@
-import os 
+import os
+import ast
 import sys
+import argparse
 from types import ModuleType
 from importlib import import_module
 from importlib.abc import MetaPathFinder, Loader
 from importlib.machinery import ModuleSpec
 from importlib.util import module_from_spec
+
+
+class _Undefined(object):
+    pass
+
+
+Undefined = _Undefined()
 
 
 class Bunch(dict):
@@ -15,20 +24,20 @@ class Bunch(dict):
             return self.__getitem__(key)
         except KeyError:
             raise AttributeError(key)
-    
+
     def __setattr__(self, key, value):
         self.__setitem__(key, value)
 
     def __delattr__(self, key):
         self.__delitem__(key)
-    
+
     def __dir__(self):
         names = dir({})
         names.extend(self.keys())
         return names
 
     def __repr__(self):
-        return "%s(%r)" % (type(self).__name__, dict(self))
+        return "%s(%s)" % (type(self).__name__, repr(dict(self))[1:-1])
 
 
 class GlobalSettings(Bunch):
@@ -49,7 +58,7 @@ class GlobalSettings(Bunch):
                 self = parent[names[-1]]
             else:
                 parent[names[-1]] = self
-        
+
         return self
 
     def __init__(self, name=None, settings=None, parent=None):
@@ -66,12 +75,9 @@ class GlobalSettings(Bunch):
         if name in self:
             return super(GlobalSettings, self).__getitem__(name)
         elif name.startswith("_") and name.endswith("_"):
-            raise AttributeError("%r" % name)
+            raise KeyError("%r" % name)
         else:
             return GlobalSettings(name, parent=self)
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__name__ or type(self).__name__, self.copy())
 
     def localize(self):
         return Settings({k : (v.localize()
@@ -126,7 +132,7 @@ def to_fullname(name, package=None):
 def import_global_settings(name=None, from_name=None, package=None):
     if name is None:
         module = GLOBAL
-    else: 
+    else:
         module = import_module(to_fullname(name, package))
     if from_name is not None:
         return getattr(module, from_name)
@@ -138,25 +144,19 @@ def import_local_settings(name=None, from_name=None):
     return import_global_settings(name, from_name).localize()
 
 
-def global_settings(name=None, from_name=None, package=None):
-    if name is None:
-        return GLOBAL
-    elif to_fullname(name, package) in sys.modules:
-        module = import_global_settings(name, package=package)
-        if from_name is not None:
-            if hasattr(module, from_name):
-                return getattr(module, from_name)
-            else:
-                return None
-        else:
-            return module
-    else:
-        return None
+def global_settings(*names):
+    settings = GLOBAL
+    for name in ".".join(names).split("."):
+        try:
+            settings = settings[name]
+        except KeyError:
+            return None
+    return settings
 
 
-def local_settings(name=None, from_name=None, package=None):
-    settings = global_settings(name, from_name, package)
-    return Settings() if settings is None else settings.localize()
+def local_settings(*names):
+    settings = global_settings(*names)
+    return settings.localize() if settings else Settings()
 
 
 class Settings(Bunch):
@@ -174,38 +174,101 @@ class Settings(Bunch):
         if "." not in name:
             return super(Settings, self).__getitem__(name)
         else:
-            setting = self
+            settings = self
             names = name.split(".")
             for i in range(len(names)):
-                setting = dict.__getitem__(setting, n)
-                if not isinstance(setting, Settings):
+                settings = settings[names[i]]
+                if not isinstance(settings, Settings):
                     raise KeyError(".".join(names[:i + 1]))
-            return setting
+            return settings
 
     def __setitem__(self, name, value):
         if "." not in name:
             super(Settings, self).__setitem__(name, value)
         else:
-            names = name.split(".")
-            self[names[:-1]][names[-1]] = value
+            name, ending = name.rsplit(".", 1)
+            self.get(name)[ending] = value
 
     def __delitem__(self, name):
         if "." not in name:
             super(Settings, self).__delitem__(name)
         else:
-            names = name.split(".")
-            del self[names[:-1]][names[-1]]
+            name, ending = name.rsplit(".", 1)
+            del self[name][ending]
 
-    def configure(self, obj):
-        for k, v in self.items():
+    def get(self, name, default=Undefined):
+        if "." not in name:
+            if name in self:
+                return self[name]
+            elif default is Undefined:
+                settings = Settings()
+                self[name] = settings
+                return settings
+            else:
+                return default
+        else:
+            settings = self
+            names = name.split(".")
+            for n in names:
+                settings = settings.get(n)
+            return settings
+
+
+    def configure(self, obj, keys=None):
+        for k in (keys or self):
+            v = self[k]
             if isinstance(v, Settings):
                 v.configure(getattr(obj, k))
             else:
                 setattr(obj, k, v)
 
-    def merge(self, other):
-        for k, v in other.items():
-            if isinstance(v, Settings) and isinstance(self[k], Settings):
+    def merge(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).items():
+            if isinstance(v, Settings) and isinstance(self.get(k), Settings):
                 self[k].merge(v)
             else:
                 self[k] = v
+
+    @classmethod
+    def from_dictionary(cls, mapping):
+        new = cls()
+        for k in mapping:
+            _new = new
+            v = mapping[k]
+            if "." in k:
+                for subk in k.split("."):
+                    _new = _new.setdefault(subk, {})
+            if isinstance(v, dict):
+                v = cls.from_mapping(v)
+            _new[k] = v
+        return new
+
+    def __repr__(self):
+        return repr(dict(self))
+
+
+class ArgumentParser(argparse.ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        super(ArgumentParser, self).__init__(*args, **kwargs)
+        self._settings = []
+
+    def add_settings(self, name, from_name=None, package=None, *args, **kwargs):
+        fullname = name
+        if package is not None:
+            fullname = "%s.%s" % (package, fullname)
+        if from_name is not None:
+            fullname = "%s.%s" % (fullname, from_name)
+        self.add_argument("--" + fullname, dest="_" + fullname.replace(".", "_"),
+            help="settings for %r" % fullname, *args, **kwargs, nargs="+")
+        self._settings.append(fullname)
+
+    def parse_settings(self, *args, **kwargs):
+        settings = Settings()
+        ns = super(ArgumentParser, self).parse_args(*args, **kwargs)
+        for name in self._settings:
+            args = getattr(ns, "_" + name.replace(".", "_"))
+            for arg in args:
+                k, v = arg.split("=")
+                settings[k] = ast.literal_eval(v)
+        return settings
